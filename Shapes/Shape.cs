5dcc9ab25f8e28c9996d9765519f5548bf02ca70
Ömer Birler <omer.birler@outlook.com>
2024-07-02 08:26:42 +0300

    Shape methods are now abstract.

--- Shapes/Shape.cs
+++ Shapes/Shape.cs
@@ -6,19 +6,15 @@ namespace Shapes
     {
         protected double Area { get; set; }
         protected double Perimeter { get; set; }
-        public virtual double CalculateArea()
-        {
-            // Calculate the area of the shape
-            // TODO: Implement the logic here
-            // Replace the line below with the actual implementation
-            return 0;
-        }
-        public virtual double CalculatePerimeter()
-        {
-            // Calculate the perimeter of the shape
-            // TODO: Implement the logic here
-            // Replace the line below with the actual implementation
-            return 0;
-        }
+
+        // Calculate the area of the shape
+        // TODO: Implement the logic here
+        // Replace the line below with the actual implementation
+        public abstract double CalculateArea();
+
+        // Calculate the perimeter of the shape
+        // TODO: Implement the logic here
+        // Replace the line below with the actual implementation
+        public virtual double CalculatePerimeter();
     }
 }
